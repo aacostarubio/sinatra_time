302f2bccd3755cf4a689b52669cfeb67f3061add
alejandro acosta-rubio <aacostarubio@gmail.com>
2013-03-26 15:15:54 -0500

    firs commit

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/assetpack'
+
+
+register Sinatra::AssetPack
+
+
+assets do
+
+  js :main, '/js/main.js', [
+  '/js/vendor/jquery*.js',
+  '/js/vendor/bootstrap-transition.js',
+  '/js/vendor/bootstrap-alert.js',
+  '/js/vendor/bootstrap-button.js',
+  #'/js/vendor/bootstrap-carousel.js',
+  '/js/vendor/bootstrap-collapse.js',
+  '/js/vendor/bootstrap-dropdown.js',
+  #'/js/vendor/bootstrap-modal.js', 
+  #'/js/vendor/bootstrap-tooltip.js',
+  #'/js/vendor/bootstrap-popover.js', 
+  #'/js/vendor/bootstrap-scrollspy.js', 
+  #'/js/vendor/bootstrap-tab.js',
+  #'/js/vendor/bootstrap-typeahead.js', 
+  #'/js/vendor/bootstrap-affix.js',
+  ]
+
+  css :bootstrap, [
+    '/css/bootstrap.min.css',
+    '/css/style.css'
+  ]
+
+  prebuild true
+
+ end
+
+
+
+
+get '/' do
+  erb :index
+end
+
+
+
+
+
