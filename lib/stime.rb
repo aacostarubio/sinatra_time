eb7888b8d4cd3ee773d60f98c34339e79cd5253a
alejandro acosta-rubio <aacostarubio@gmail.com>
2013-04-08 15:45:54 -0500

    test for year first

--- lib/stime.rb
+++ lib/stime.rb
@@ -1,17 +1,25 @@
 class Stime
 
+ def initialize
+   @letter_from_month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Sep", "Oct", "Nov", "Dec"]
+   @word_from_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+ end
+
  def date_format(input)
-  letter_from_month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Sep", "Oct", "Nov", "Dec"]
-  word_from_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  abreviacion_del_mes = "time.strftime" + "('%b" + " %-d" + " %Y')"
+  complete_word = "time.strftime" + "('%B" + " %-d" + " %Y')"
+  year_complete = ["time.strftime" + "('%Y " + "%-d" + " %-m')",
+                   "time.strftime" + "('%Y " + "%-m" + " %-d')"]
 
   new_time = input
   new_time.capitalize!
 
-  if letter_from_month.include? new_time.split[0]
-    return "time.strftime" + "(%b" + " %-d" + " %Y)"
-  elsif word_from_month.include? new_time.split[0]
-    return "time.strftime" + "(%B" + " %-d" + " %Y)"
-  end
+  return abreviacion_del_mes if (@letter_from_month.include? new_time.split[0])
+  return complete_word if (@word_from_month.include? new_time.split[0])
+  return year_complete[0] if (new_time.split[0].length == 4) && (new_time.split[1].to_i > 12)
+  return year_complete[1] if (new_time.split[0].length == 4) && (new_time.split[1].to_i <= 12)
 
  end
 
--- spec/stime_spec.rb
+++ spec/stime_spec.rb
@@ -2,19 +2,28 @@ require 'stime'
 
 describe Stime do
 
+  before :each do
+   @a = Stime.new
+  end
+
  it 'should return the strftime code if the user inputs the abbreviation of the month' do
-  @a = Stime.new
-  @a.date_format("Oct 14 1992").should == "time.strftime" + "(%b" + " %-d" + " %Y)" 
+  @a.date_format("Oct 14 1992").should == "time.strftime" + "('%b" + " %-d" + " %Y')"
  end
 
  it 'should return the strftime code if the user inputs the months complete word' do
-  @a = Stime.new
-  @a.date_format("October 14 1992").should == "time.strftime" + "(%B" + " %-d" + " %Y)"
+  @a.date_format("October 14 1992").should == "time.strftime" + "('%B" + " %-d" + " %Y')"
  end
 
  it 'should return the strftime code if the user inputs the months in lowercase' do
-  @a = Stime.new
-  @a.date_format("october 14 1992").should == "time.strftime" + "(%B" + " %-d" + " %Y)"
+  @a.date_format("october 14 1992").should == "time.strftime" + "('%B" + " %-d" + " %Y')"
+ end
+
+ it 'should return a different strftime for when the year is first' do
+  @a.date_format("1992 14 10").should  == "time.strftime" + "('%Y " + "%-d" + " %-m')"
+ end
+
+ it 'should return strftime code for when the month and year are less than 12' do
+  @a.date_format("1992 10 11").should == "time.strftime" + "('%Y " + "%-m" + " %-d')"
  end
 
 end
